5c1c3d06fb3d3ad11cfb0763e93bab7b486d5a13
Mason Meyer <mason.meyer@gmail.com>
2025-03-04 10:17:10 -0800

    added transition effects for the projects section

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import styles from './Projects.Styles.module.css';
 import ProjectCard from '../../common/ProjectCard';
 import dataWizard from '../../assets/dataWizardLogo.webp';
@@ -5,41 +6,84 @@ import adventureMatch from '../../assets/adventureBuddyLogo.webp';
 import liftMetrics from '../../assets/liftMetrics.webp';
 import underConstruction from '../../assets/underConstruction.webp';
 
-//make an array of project data and then map over and create project cards
-
 function Projects() {
+  const cardRefs = useRef([]);
+
+  useEffect(() => {
+    const handleIntersection = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(styles.visible);
+        } else {
+          entry.target.classList.remove(styles.visible);
+        }
+      });
+    };
+    const observer = new IntersectionObserver(handleIntersection, {
+      threshold: 0.5,
+    });
+
+    cardRefs.current.forEach((card) => {
+      if (card) observer.observe(card);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
     <section id='projects' className={styles.container}>
       <h1 className='sectionTitle'>Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard
-          src={dataWizard}
-          link='https://github.com/mmeyer23/dataWizard'
-          alt='DataWizard Logo'
-          name='DataWizard'
-          description='A.I. Database Population Tool'
-        />
-        <ProjectCard
-          src={adventureMatch}
-          link='https://github.com/mmeyer23/AdventureMatch'
-          alt='Adventure Match Logo'
-          name='Adventure Match'
-          description='An App for Outdoor Enthusiasts'
-        />
-        <ProjectCard
-          src={liftMetrics}
-          link='https://www.github.com'
-          alt='LiftMetrics Logo'
-          name='LiftMetrics'
-          description='Personalized Workout Tracker'
-        />
-        <ProjectCard
-          src={underConstruction}
-          link='https://www.github.com'
-          alt='Under Contruction Logo'
-          name='In Progress'
-          description='Coming Soon!'
-        />
+        <div
+          ref={(el) => (cardRefs.current[0] = el)}
+          className={styles.projectCard}
+        >
+          <ProjectCard
+            src={dataWizard}
+            link='https://github.com/mmeyer23/dataWizard'
+            alt='DataWizard Logo'
+            name='DataWizard'
+            description='A.I. Database Population Tool'
+          />
+        </div>
+        <div
+          ref={(el) => (cardRefs.current[1] = el)}
+          className={styles.projectCard}
+        >
+          <ProjectCard
+            src={adventureMatch}
+            link='https://github.com/mmeyer23/AdventureMatch'
+            alt='Adventure Match Logo'
+            name='Adventure Match'
+            description='An App for Outdoor Enthusiasts'
+          />
+        </div>
+        <div
+          ref={(el) => (cardRefs.current[2] = el)}
+          className={styles.projectCard}
+        >
+          <ProjectCard
+            src={liftMetrics}
+            link='https://www.github.com'
+            alt='LiftMetrics Logo'
+            name='LiftMetrics'
+            description='Personalized Workout Tracker'
+          />
+        </div>
+        <div
+          ref={(el) => (cardRefs.current[3] = el)}
+          className={styles.projectCard}
+        >
+          <ProjectCard
+            src={underConstruction}
+            link='https://www.github.com'
+            alt='Under Contruction Logo'
+            name='In Progress'
+            description='Coming Soon!'
+          />
+        </div>
       </div>
     </section>
   );
